299472a745c9a9110ff82c05aadd8c5cbd1762c4
st10275468 <st10275468@vcconnect.edu.za>
2024-05-30 12:58:57 +0200

    Created a unit test for the way I am calculating the total calories.

---
+++ st10275468_UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("st10275468_UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("st10275468_UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ba774db0-ad67-48be-896c-74dc8d37e636")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ st10275468_UnitTests/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using st10275468_PROG6221_PoePart1_ThomasK_Gr03;
+namespace st10275468_UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestTotalCalories()
+        {
+            var recipe = new Recipe("Test");
+
+            var recipeIngredient1 = new Ingredient("Ingredient 1", 200,"ml", "Meat", 200);
+            var recipeIngredient2 = new Ingredient("Ingredient 2", 2, "Spoons", "Dairy products", 100);
+            recipe.AddIngredient(recipeIngredient1);
+            recipe.AddIngredient(recipeIngredient2);
+
+           
+            Assert.AreEqual(300, recipe.recipeIngredients[0].ingredientCalories + recipe.recipeIngredients[1].ingredientCalories);
+        }
+    }
+}
